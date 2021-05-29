acc92b2c24750ed121f4a071f8c483e9034a65e9
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-05-29 10:36:05 +0700

    menambahkan Barang

---
+++ MyPOSApps/Models/Barang.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPOSApps.Models
+{
+    public class Barang
+    {
+        public string KodeBarang { get; set; }
+        public string NamaBarang { get; set; }
+        public decimal HargaBeli { get; set; }
+        public decimal HargaJual { get; set; }
+        public int Stok { get; set; }
+        public DateTime TanggalBeli { get; set; }
+    }
+}
